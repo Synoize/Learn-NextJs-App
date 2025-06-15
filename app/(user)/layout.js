904de1ec07e0ec0f6d38724a0ac652e983e0aa12
synoize <suryavanshivam@gamil.com>
2025-06-15 12:38:26 +0530

    2 Day : NextJs_App: [Font Optimization]

--- app/(user)/layout.js
+++ app/(user)/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "../globals.css";
 import Navigation from "@/components/Navigation";
 
-const geistSans = Geist({
+const outfit = Outfit({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -21,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}`}
+        className={outfit.className}
       >
         <Navigation />
         <div className='max-w-7xl mx-auto p-8'>
